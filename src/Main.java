796960764a6d26b8ec986270d044e454c29f0323
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2022-08-29 05:51:15 -0500

    accidentally skipped to hour mark, for loop examples are out of place

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+/*
         try {
             FileWriter writer = new FileWriter("poem.txt");
             writer.write("Roses are red \nViolets are blue \n");
@@ -15,5 +17,24 @@ public class Main {
 
             e.printStackTrace();
         }
+*/
+
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        String symbol = "";
+
+        System.out.println("Enter # of rows: ");
+        rows = scanner.nextInt();
+        System.out.println("Enter # of columns: ");
+        columns = scanner.nextInt();
+        System.out.println("Enter symbol to use: ");
+        symbol = scanner.next();
+
+        for(int i=1; i<=rows; i++) {
+            System.out.println();
+            for(int j=1; j<=columns; j++) {
+                System.out.print(symbol);
+            }
     }
-}
+}}
